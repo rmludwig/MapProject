5e272aa483e7a5f55ecc3fc22c758aec9f0271d1
Richard Ludwig <rmludwig@rmlsn.net>
2015-06-12 23:56:28 -0500

    Infor window now has content. Ready for cleanup.

--- js/neighborhood.js
+++ js/neighborhood.js
@@ -2,85 +2,103 @@
 /* Location data for my markers */
 var LocationData = [
     {
-        name : 'Home',
+        name : 'Rich\'s Home',
+        desc : 'Where Rich sleeps, works, plays, and studies with Udacity.',
         lat : 38.7333260,
         lng : -90.5884440,
         id : 1,
         visible : true,
         latLng : "",
-        marker : ""
+        marker : "",
+        searchString : "home"
     },
     {
         name : 'Shop N Save',
+        desc : 'The local super marker where Rich buys fresh avecados.',
         lat : 38.7464160,
         lng : -90.5763050,
         id : 2,
         visible : true,
         latLng : "",
-        marker : ""
+        marker : "",
+        searchString : "grocery"
     },
     {
         name : 'Jim\'s House',
+        desc : 'Where Rich plays poker with the guys on Thurseday nights.',
         lat : 38.7304430,
         lng : -90.5909640,
         id : 3,
         visible : true,
         latLng : "",
-        marker : ""
+        marker : "",
+        searchString : "poker game"
     },
     {
         name : 'Grade School',
+        desc : 'Where all the kids go to grade school.',
         lat : 38.7271690,
         lng : -90.5983220,
         id : 4,
         visible : true,
         latLng : "",
-        marker : ""
+        marker : "",
+        searchString : "grade school"
     },
     {
         name : 'Bus Stop',
+        desc : 'Where the kids catch the bus ride to get to grade school.',
         lat : 38.7347680,
         lng : -90.5882060,
         id : 5,
         visible : true,
         latLng : "",
-        marker : ""
+        marker : "",
+        searchString : "school bus"
     },
     {
         name : 'Pretzel Stop',
+        desc : 'Rich\'s favorite place to get a yummy pretzel snack.',
         lat : 38.7448890,
         lng : -90.5922510,
         id : 6,
         visible : true,
         latLng : "",
-        marker : ""
+        marker : "",
+        searchString : "fresh pretzel"
     },
     {
         name : 'Shell Gas Station',
+        desc : 'Where Rich goes to fill up the gas guzzler and grab a quick coffee.',
         lat : 38.7449030,
         lng : -90.5810290,
         id : 7,
         visible : true,
         latLng : "",
-        marker : ""
+        marker : "",
+        searchString : "gasoline"
     },
     {
         name : 'Dog Walk Turnaround',
+        desc : 'A good place to turn around and head back when on a dog walk.',
         lat : 38.7198040,
         lng : -90.5796640,
         id : 8,
         visible : true,
         latLng : "",
-        marker : ""
+        marker : "",
+        searchString : "puppy"
     },
     {
         name : 'Zion Church',
+        desc : 'The local soccer fields for the kids.',
         lat : 38.7424640,
         lng : -90.5843770,
         id : 9,
         visible : true,
         latLng : "",
-        marker : ""
+        marker : "",
+        searchString : "soccer"
     }
 ]; 
 
@@ -90,12 +108,14 @@ var LocationData = [
 var Location = function(data) {
     // Attributes OR observables
     this.name = data.name;
+    this.desc = data.desc;
     this.lat = data.lat;
     this.lng = data.lng;
     this.id = data.id;
     this.visible = ko.observable(data.visible);
     this.latLng = data.latLng;
     this.marker = data.marker;
+    this.searchString = data.searchString;
 };
 
 
@@ -174,10 +194,14 @@ var ViewModel = function() {
 
     };
 
+
     // Create info window for overlay elements
     function loadOverlay() {
         // For each location build an initial marker using visibility attribute
         for (var i = 0; i < self.locationList().length; i++) { 
+
+            // Prepare the content.
+            var content = buildMarkerPopContent(self.locationList()[i]);
 
             // Create latLng then set to the array attribute on locations
             var position = new google.maps.LatLng(self.locationList()[i].lat, self.locationList()[i].lng)
@@ -188,13 +212,14 @@ var ViewModel = function() {
                 map: self.map
             });
 
-            google.maps.event.addListener(self.locationList()[i].marker, 'click', (function(thisMarker, i) {
+            google.maps.event.addListener(self.locationList()[i].marker, 'click', (function(thisMarker, i, content) {
                 return function() {
-                    self.iWindow.setContent(self.locationList()[i].name);
+                    self.iWindow.setContent(content);
                     self.iWindow.open(self.map, thisMarker);
+                    fetchWikiInfo(self.locationList()[i]);
+                    fetchFlickrInfo(self.locationList()[i]);
                 }
-            })(self.locationList()[i].marker, i));
-
+            })(self.locationList()[i].marker, i, content));
         };
     };
 
@@ -211,18 +236,132 @@ var ViewModel = function() {
         });
     }
 
+    // Build location specific content container
+    function buildMarkerPopContent(listItem) {
+        // create elements for the popup
+        var container;
+        container += '<div class="popup-container">';
+        container += '<h2>'+listItem.name+'</h2>';
+        container += '<p>'+listItem.desc+'</p>';
+        container += '<h4>Wikipedia Links</h4>';
+        container += '<ul id="wiki-list-'+listItem.id+'" class="wiki-list"></ul>';
+        container += '<img id="flickr-image-'+listItem.id+'" class="flickr-image" alt="' + listItem.searchString + '"/>';
+        container += '</div>';
+        
+        // return the string
+        return container;
+    }
+
+
+    function fetchWikiInfo(listItem) {
+        // Get the element that I hope to modify via ajax
+        var wikiListUl = $('#wiki-list-'+listItem.id);
+
+        // Make a timeout for uncought errors with JSONP callback to wiki
+        var wikiRequestTimeout = setTimeout(function(){
+            wikiListUl.append("<li>Failed to load Wikipedia links!</li>");
+        // 8 second timeout
+        }, 8000);
+
+        // set attributes for JSONP request with callback
+        var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + listItem.searchString + '&format=json&callback=wikiCallback';
+        var ajaxSettings = {url: wikiURL,
+                            dataType: "jsonp",
+                            success: function(response) {
+                                        console.log("Wiki Callback");
+                                        console.log(response);
+                                        var articleList = response[1];
+                                        for (var i = 0; i < 3; i++) {
+                                            articleStr = articleList[i];
+                                            var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                                            console.log(url);
+                                            wikiListUl.append('<li class="wiki-item"><a href="' + url + '">' + articleStr + '</a></li>');
+                                        };
+
+                                        // Prevent timeout above from overwriting if success
+                                        clearTimeout(wikiRequestTimeout);
+                                     }
+                            };
+        $.ajax(ajaxSettings)
+        .error(function(error) {
+            wikiListUl.append("<li>Wikipedia Links Couldn't Load!</li>");
+        });
+    };
+
+
+    function fetchFlickrInfo(listItem) {
+        // Get the element that I will pupulate with json result
+        var flickrImg = $('#flickr-image-'+listItem.id);
+
+        // Make a timeout for uncought errors with JSONP callback to wiki
+        var flickrRequestTimeout = setTimeout(function(){
+            flickrImg.replaceWith("<div>Failed to load Flickr image!</div>");
+        // 8 second timeout
+        }, 8000);
+
+        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+        {
+          tags: listItem.searchString,
+          tagmode: "any",
+          format: "json"
+        },
+        function(data) {
+          $.each(data.items, function(i,item){
+            flickrImg.attr("src", item.media.m);
+            if ( i == 2 ) return false;
+          });
+                // Prevent timeout above from overwriting if success
+                clearTimeout(flickrRequestTimeout);
+
+
+        });
+
+/*
+        // set attributes for JSONP request with callback
+        var flickrURL = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?'
+        var ajaxSettings = {url: flickrURL,
+                            dataType: "jsonp",
+                            success: function(response) {
+                                        console.log("Flickr Callback");
+                                        console.log(response);
+                                        //
+                                        //
+                                        //
+                                        var imageList = response[1];
+                                        for (var i = 0; i < 3; i++) {
+                                            articleStr = articleList[i];
+                                            var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                                            console.log(url);
+                                            wikiListUl.append('<li class="wiki-item"><a href="' + url + '">' + articleStr + '</a></li>');
+                                        };
+                                        //
+                                        //
+
+                                        // Prevent timeout above from overwriting if success
+                                        clearTimeout(flickrRequestTimeout);
+                                     }
+                            };
+        $.ajax(ajaxSettings)
+        .error(function(error) {
+            flickrImg.replaceWith("<div>Flickr content did not load!</div>";
+        });
+*/
+
+
+    };
+
     // Makes all markers hidden, resets search input, and makes 
     // ONLY the selected marker visible
     self.selectItemFromList = function(item) {
         changeAllVisibility(false);
         item.marker.setMap(self.map);
         item.visible(true);
-    }
+    };
 
     // Makes all markers visible and resets search input
     self.makeAllVisible = function() {
         changeAllVisibility(true);
-    }
+    };
     
     // On instatiation of VeiwMoeld initialize
     google.maps.event.addDomListener(window, 'load', initialize);
